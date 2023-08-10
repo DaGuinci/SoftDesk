76a734faca2247c98a06e99dea141a02e97fb7c6
Semsei <guillaume@36pixels.fr>
2023-08-10 14:54:39 +0200

    feat - user admin without token

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from authentication.models import User
 from authentication.serializers import UserSerializer
+from authentication.permissions import IsAdminAuthenticated
 
-class UserViewset(ModelViewSet):
+
+class UserViewset(ReadOnlyModelViewSet):
+
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.all()
+
+
+class AdminUserViewset(ModelViewSet):
+
+    permission_classes = [IsAdminAuthenticated]
 
     serializer_class = UserSerializer
 
--- softDesk/urls.py
+++ softDesk/urls.py
@@ -18,10 +18,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from authentication.views import UserViewset
+from authentication.views import UserViewset, AdminUserViewset
 
 router = routers.SimpleRouter()
 router.register('user', UserViewset, basename='user')
+router.register('admin/user', AdminUserViewset, basename='admin-user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
